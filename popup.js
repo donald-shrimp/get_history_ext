9c88bdb638394aef466d046b97ca56d627fdb30f
donald-shrimp <t0706244@gmail.com>
2021-10-11 15:06:07 +0900

    とりあえず学生番号を保存するところはできた

--- popup.js
+++ popup.js
@@ -1,6 +1,14 @@
 console.log("うんち");
-function use_start() {
-    let uid = document.getElementById('stunum');
+
+//idが登録されていた場合は入力バーに入れておく
+let defaultid = localStorage.getItem('uid');
+if (defaultid != null) {
+    document.getElementById("stunum").value = defaultid ;
+}
+
+//利用開始ボタンが押されたら値を取得しlocalstorageに渡す(拡張機能のローカルストレージは「ポップアップを検証」のところからしか確認できないため注意)
+document.getElementById("use_start").onclick = function() {
+    let uid = document.getElementById('stunum').value;
     console.log(uid);
-    localStorage.uid = uid;
+    localStorage.setItem('uid', uid);
 }
