e77ce326046cb65a75f2b66ba3d02b17e726a4c3
kazuki19992 <kushida98@gmail.com>
2021-11-05 15:34:40 +0900

    コード量の削減とデータベースへの送信

--- background.js
+++ background.js
@@ -1,55 +1,30 @@
-// chrome.runtime.onInstalled.addListener(function () {
-//   chrome.storage.sync.set({ color: '#3aa757' }, function () {
-//     console.log('The color is green.');
-//   });
-
-//   // chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-//   //   chrome.declarativeContent.onPageChanged.addRules([{
-//   //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-//   //       pageUrl: { hostEquals: 'developer.chrome.com' },
-//   //     })
-//   //     ],
-//   //     actions: [new chrome.declarativeContent.ShowPageAction()]
-//   //   }]);
-//   // });
-
-// });
-
 //履歴を取得してコンソールに出す
-
-
 chrome.history.onVisited.addListener((result) => {
-  // var tab_title;
-  
-  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  //   var tab = tabs[0];
-  //   var title = tab.title;
-  //   console.log("Title: " + title);
-  //   tab_title = title;
-    
-  // });
-
   const historyItem = result;
   console.log(historyItem.title);
   console.log(historyItem.url);
   console.log(new Date(historyItem.lastVisitTime));
   console.log(localStorage.getItem('uid'));
 
-  const json = JSON.stringify({
-                              "title":historyItem.title,
-                              "url":historyItem.url,
-                              "date":new Date(historyItem.lastVisitTime),
-                              "uid":localStorage.getItem('uid') 
-                              });
-  
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+
+  const json = JSON.stringify(
+    {
+      "title":historyItem.title,
+      "url":historyItem.url,
+      "date":new Date(historyItem.lastVisitTime),
+      "uid":localStorage.getItem('uid')
+    }
+  );
+
   console.log(json)
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:8080",
-    dataType: "json",
-    contentType: "application/json",
-    data: json
-})
+
+  fetch("http://localhost:8080", {method: 'post', headers: headers, body: json}).then((res) => {
+    res.text().then(console.log)
+  })
 
 })
 
