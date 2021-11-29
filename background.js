d57695ee426245610db1a08559f3f125b50f1871
donald-shrimp <t0706244@gmail.com>
2021-11-29 17:06:00 +0900

    みんなが見るとこ(youtubeのホーム)とかは除外した

--- background.js
+++ background.js
@@ -23,16 +23,20 @@ chrome.history.onVisited.addListener(async(result) => {
     'Content-Type': 'application/json'
   }
 
-  //tabの情報を取得(historyItemをつかうのはやめた)
   
+  //tabの情報を取得()
 　const title = await getTitle();
   console.log(historyItem.title);
   console.log(historyItem.url);
   console.log(new Date(historyItem.lastVisitTime));
   console.log(localStorage.getItem('uid'));
 
-  if (localStorage.getItem('url') !== historyItem.url||localStorage.getItem('title') !== historyItem.title) { //localstorageに格納された値と違うURLを取得した場合のみJSON送信
-    console.log("ちがうよ")
+  if (localStorage.getItem('url') === historyItem.url||localStorage.getItem('title') === historyItem.title) { //localstorageに格納された値と違うURLを取得した場合のみJSON送信
+    console.log("おなじだよ")
+  }else if(historyItem.url===''||historyItem.url==='https://music.amazon.co.jp/'||historyItem.url==='https://www.youtube.com/'||historyItem.url==='https://www.youtube.com/?gl=JP&hl=ja'||historyItem.title==='Amazon | 本, ファッション, 家電から食品まで | アマゾン'||historyItem.url==='https://www.google.com/'||historyItem.url==='https://www.yahoo.co.jp/'||historyItem.url===''){ 
+    console.log("送信しないよ")
+  }else {
+    console.log("送信するよ")
     const json = JSON.stringify(
       {
         "title": historyItem.title,
@@ -52,8 +56,6 @@ chrome.history.onVisited.addListener(async(result) => {
     //新たなURLを設定
     localStorage.setItem('url', historyItem.url);
     localStorage.setItem('title', historyItem.title);
-  } else {
-    console.log("おなじだよ")
   }
 
 })
